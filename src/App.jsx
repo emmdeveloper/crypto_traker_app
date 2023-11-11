ef7f7626ce8645779b2deb15371fbaaf4c2d2243
emmdeveloper <keshichidera@gmail.com>
2023-11-11 16:12:18 +0100

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
  */
 import Navbar from "./components/Navbar";
 import CoinList from "./components/CoinList";
-import { Route, Routes } from "react-router";
+
 import Hero from "./components/Hero";
-import Coin from "./routes/Coin";
+
 import Features from "./components/Features/Features";
-import About from "./components/About/aBOUT.JSX";
+import About from "./components/About/About";
 import Reviews from "./components/Reviews/Reviews";
 import GetNow from "./components/GetNow/GetNow";
 import Footer from "./components/Footer";
